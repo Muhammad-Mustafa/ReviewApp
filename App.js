dfec9e0169cac6b90d6806bab2b25ccdb7a8d823
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-08-23 21:20:12 +0500

    initial project

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>Yo Sup Basic React Native App</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
